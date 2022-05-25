14d47eafbc53afa5117e3edd04dfb3255218d95b
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-05-25 18:05:00 +0530

    env big fix

--- app.js
+++ app.js
@@ -81,5 +81,5 @@ app.use('*',(err,req,res,next)=>{
 })
 
 app.listen(process.env.PORT,()=>{
-    console.log(`server is running at port ${5000}` )
+    console.log(`server is running at port ${process.env.PORT}` )
 })
